a11981ce1dcff89d19a28d08d769cbe0458995f3
joao-o <joao_d3a50@hotmail.com>
2014-01-10 20:24:31 +0000

    agora não crasha ('')d

--- miscui.c
+++ miscui.c
@@ -38,7 +38,7 @@ erroluneta (progdata * dat)
 }
 
 gboolean
-colorselec (progdata *pdat)
+colorselec (GtkWidget widget, progdata *pdat)
 {
   int i;
   i = gtk_combo_box_get_active(GTK_COMBO_BOX(pdat->combocolor));
@@ -56,23 +56,25 @@ colorselec (progdata *pdat)
 gboolean
 colorchange (GtkWidget * widget, progdata *pdat)
 {
-  if (GTK_OBJECT (widget) == pdat->rgbscl[0].adj)
+ /*
+ if (GTK_OBJECT (widget) == pdat->rgbscl[0].adj)
     pdat->ptclr->red = (GTK_ADJUSTMENT(pdat->rgbscl[0].adj))->value*255;
  if (GTK_OBJECT (widget) == pdat->rgbscl[0].adj)
     pdat->ptclr->green = (GTK_ADJUSTMENT(pdat->rgbscl[1].adj))->value*255;
  if (GTK_OBJECT (widget) == pdat->rgbscl[0].adj)
     pdat->ptclr->blue = (GTK_ADJUSTMENT(pdat->rgbscl[2].adj))->value*255;
  printf("%d",pdat->ptclr->blue);
+ */
 
  return TRUE;
 
 }
 gboolean
-colormenu (progdata *dat)
+colormenu (GtkWidget *widget, gpointer dat)
 {
   progdata *pdat = (progdata *) dat;
-  GtkWidget *dialog, *btn, *tbl, *rgbbox, *rgbfrm, *rgbbar[3];
-  GdkColor base[0];
+  GtkWidget *dialog, *btn, *mainbox, *box2, *rgbbox, *rgbfrm, *rgbbar[3];
+  GdkColor base[3];
   int i;
 
   dialog = gtk_dialog_new_with_buttons ("Menu de Cores",
@@ -82,15 +84,12 @@ colormenu (progdata *dat)
                                          GTK_RESPONSE_NONE,
                                          NULL);
 
-  tbl = gtk_table_new (3 , 2, TRUE);
-  gtk_table_set_row_spacings(GTK_TABLE(tbl), 5);
-  gtk_table_set_col_spacings(GTK_TABLE(tbl), 5);
-  gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox), tbl);
+  mainbox = gtk_hbox_new(FALSE,0);
+  gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox),mainbox);
 
   rgbbox =  gtk_hbox_new (FALSE, 0);
   rgbfrm = gtk_frame_new ("RGB");
-  gtk_table_attach_defaults (GTK_TABLE (tbl), rgbfrm, 
-			     1, 2, 0, 3);
+  gtk_box_pack_start(GTK_BOX(mainbox),rgbfrm,TRUE,TRUE,0);
   gtk_container_add (GTK_CONTAINER (rgbfrm), rgbbox);
 
   pdat->combocolor = gtk_combo_box_text_new();
@@ -106,29 +105,35 @@ colormenu (progdata *dat)
 				 "Raios Reais");
   gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(pdat->combocolor),
 				 "Raios Virtuais");
-
-  gtk_table_attach_defaults (GTK_TABLE (tbl), pdat->combocolor, 
-			     0, 1, 0, 1);
   
+  box2 = gtk_vbox_new (FALSE,0);
+  gtk_box_pack_start(GTK_BOX(box2),pdat->combocolor,TRUE,TRUE,0);
   btn = gtk_toggle_button_new_with_label ("Restaurar Cores");
-  gtk_table_attach_defaults (GTK_TABLE (tbl), btn, 
-			     0, 1, 2, 3);
+  gtk_box_pack_start(GTK_BOX(box2),btn,TRUE,TRUE,0);
 
+  gtk_box_pack_start(GTK_BOX(mainbox),box2,TRUE,TRUE,0);
+  
+
+
+/*
   gdk_color_parse ("red", &base[0]);
   gdk_color_parse ("green", &base[1]);
   gdk_color_parse ("blue", &base[2]);
-
+*/
   for(i=0;i<3;i++)
     {
       pdat->rgbscl[i].adj = gtk_adjustment_new (100, 0, 255, 0.01, 1.0, 1.0);
       rgbbar[i] = gtk_vscale_new (GTK_ADJUSTMENT (pdat->rgbscl[i].adj));
       gtk_container_add (GTK_CONTAINER (rgbbox), rgbbar[i]);
+      
+      /*
       gtk_widget_modify_bg (rgbbar[i],
 			    GTK_STATE_NORMAL, &base[i]);
       gtk_widget_modify_bg (rgbbar[i],
 			    GTK_STATE_PRELIGHT, &base[i]);
       gtk_widget_modify_bg (rgbbar[i],
 			    GTK_STATE_ACTIVE, &base[i]);
+      */
     }
 
   g_signal_connect (G_OBJECT (pdat->rgbscl[0].adj), "value-changed",
@@ -141,10 +146,7 @@ colormenu (progdata *dat)
   g_signal_connect (G_OBJECT (pdat->combocolor), "changed",
 		    G_CALLBACK (colorselec), pdat);
 
-  g_signal_connect (GTK_STOCK_OK, "clicked", G_CALLBACK (gtk_widget_destroy), 
-		    dialog);
-
-  gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox), tbl);
+  g_signal_connect_swapped (dialog, "response", G_CALLBACK (gtk_widget_destroy), dialog);
 
   gtk_widget_show_all (dialog);
 
